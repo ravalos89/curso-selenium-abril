6ff87b60c4f6d92e2a9c3d1b9d8d66428b34ec93
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-04-30 11:29:42 -0500

    first commit

---
+++ SeleniumCursoAbril/src/test/java/com/google/FirstTest.java
@@ -0,0 +1,21 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+	@Test
+	public void f() {
+		ChromeOptions chromeOpt = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(chromeOpt);
+
+		driver.get("https://www.google.com");
+
+		driver.close();
+	}
+}
